d1bc9896b58092b59cc907df1f7b2ecd7a0f7b00
JhonSmith <cuatrozero1@gmail.com>
2025-07-09 07:34:53 -0500

    Se termina la seccion de nosotros, y la del arena gaming, se empieza otra seccion

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
+

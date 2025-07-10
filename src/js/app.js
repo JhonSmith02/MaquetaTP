549eb6b488fefa839b21733e0f68b87d31e70f43
JhonSmith <cuatrozero1@gmail.com>
2025-07-10 00:03:57 -0500

    Se termina la seccion de sedes, creando bogota, medellin, barranquilla,

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 const swiper = new Swiper(".mySwiper", {
+   
     effect: "cards",
     grabCursor: true,
     loop: true,
@@ -8,3 +9,56 @@ const swiper = new Swiper(".mySwiper", {
     }
   });
 
+
+
+  const configs = [
+    {main: '.swiperMain', thumbs: '.swiperThumbs'},
+    {main: '.swiperMainMedellin', thumbs: '.swiperThumbsMedellin'},
+    {main: '.swiperMainBarranquilla', thumbs: '.swiperThumbsBarranquilla'}
+  ];
+
+configs.forEach(cfg => {
+  const thumbSwiper = new Swiper(cfg.thumbs, {
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,   
+      watchSlidesProgress: true,
+  });
+
+  new Swiper(cfg.main, {
+    spaceBetween: 10,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction"
+    },
+    navigation: {
+      nextEl: `${cfg.main} .swiper-button-next`,
+      prevEl: `${cfg.main} .swiper-button-prev`,
+    },
+  thumbs: {
+    swiper: thumbSwiper,
+   },
+  });
+});
+
+// // Inicializa miniaturas primero
+// const thumbSwiper = new Swiper(".swiperThumbs", {
+//   spaceBetween: 10,
+//   slidesPerView: 4,
+//   freeMode: true,
+//   watchSlidesProgress: true,
+// });
+
+// // Inicializa swiper principal y linkea con miniaturas
+// const mainSwiper = new Swiper(".swiperMain", {
+//   spaceBetween: 10,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   thumbs: {
+//     swiper: thumbSwiper,
+//   },
+//   loop: true,
+// });
